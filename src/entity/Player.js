d902fa36d35b9b3e4d6878eb60e426709dbaf9ee
Elstan Lewis <elstanlewis7@gmail.com>
2022-01-28 15:35:40 -0500

    added collisions, needs adjusting

--- src/entity/Player.js
+++ src/entity/Player.js
@@ -1,4 +1,4 @@
-import Phaser from "phaser";
+import Phaser from 'phaser';
 
 export default class Player extends Phaser.Physics.Arcade.Sprite {
   constructor(scene, x, y, spriteKey) {
@@ -6,5 +6,28 @@ export default class Player extends Phaser.Physics.Arcade.Sprite {
     this.scene = scene;
     this.scene.add.existing(this);
     this.scene.physics.world.enable(this);
+  }
+  //Movements
+  updateMovement(cursors) {
+    // this.key_W = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.W);
+    // this.key_A = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.A);
+    // this.key_S = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.S);
+    // this.key_D = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.D);
+    if (cursors.up.isDown) {
+      this.setVelocityY(-100);
+    } else if (cursors.left.isDown) {
+      this.setVelocityX(-100);
+    } else if (cursors.down.isDown) {
+      this.setVelocityY(100);
+    } else if (cursors.right.isDown) {
+      this.setVelocityX(100);
+    } else {
+      this.setVelocityY(0);
+      this.setVelocityX(0);
+    }
+  }
+
+  update(cursors) {
+    this.updateMovement(cursors);
   }
 }
--- src/scene/OverworldScene.js
+++ src/scene/OverworldScene.js
@@ -1,34 +1,52 @@
-import Phaser from "phaser";
+import Phaser from 'phaser';
+import Player from '../entity/Player';
 
 export default class OverworldScene extends Phaser.Scene {
   constructor() {
-    super("OverworldScene");
+    super('OverworldScene');
   }
+
   preload() {
-    this.load.image(
-        "tiles",
-        "assets/maps/tilemap.png"
-    );
-    this.load.tilemapTiledJSON(
-        "tilemap",
-        "assets/maps/overworldMap.json"
-    );
+    this.load.image('tiles', 'assets/maps/tilemap.png');
+    this.load.tilemapTiledJSON('tilemap', 'assets/maps/overworldMap.json');
+    //player
+    this.load.image('player', 'assets/sprites/sensei.png');
   }
   create() {
-      // Creating Map using Tile Set
-      const map = this.make.tilemap({key: "tilemap"})
-      // "characters" comes from name in Tiled software
-      const tileset = map.addTilesetImage("characters","tiles",16,16)
-      
-      // Layers
-      const waterLayer = map.createLayer("Water",tileset,0,0)
-      const groundLayer = map.createLayer("Ground",tileset,0,0)
-      const interactiveLayer = map.createLayer("Interactive",tileset,0,0)
-      const overheadLayer = map.createLayer("Overhead",tileset,0,0)
+    // Creating Map using Tile Set
+    const map = this.make.tilemap({ key: 'tilemap' });
+    // "characters" comes from name in Tiled software
+    const tileset = map.addTilesetImage('characters', 'tiles', 16, 16);
 
-      // Placeholder Camera
-      const camera = this.cameras.main
-      camera.setZoom(2)
+    // Layers
+    const waterLayer = map.createLayer('Water', tileset, 0, 0);
+    const groundLayer = map.createLayer('Ground', tileset, 0, 0);
+    const interactiveLayer = map.createLayer('Interactive', tileset, 0, 0);
+    const overheadLayer = map.createLayer('Overhead', tileset, 0, 0);
+
+    //Player
+    this.player = new Player(this, 200, 200, 'player').setScale(0.75);
+    this.cursors = this.input.keyboard.createCursorKeys();
+
+    //Collisions
+    waterLayer.setCollisionByProperty({ collides: true });
+    this.physics.add.collider(this.player, waterLayer);
+
+    groundLayer.setCollisionFromCollisionGroup({ collide: true });
+    this.physics.add.collider(this.player, groundLayer);
+
+    interactiveLayer.setCollisionByProperty({ collide: true });
+    this.physics.add.collider(this.player, interactiveLayer);
+
+    this.player.setDepth(0);
+    overheadLayer.setDepth(10);
+
+    // Placeholder Camera
+    const camera = this.cameras.main;
+    camera.setZoom(2);
+    camera.startFollow(this.player, true);
   }
-  update() {}
+  update() {
+    this.player.update(this.cursors);
+  }
 }
