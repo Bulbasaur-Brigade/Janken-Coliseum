8e76723770dfe2f8052bf79fdcbd9e40af9d6f0e
hectorcarrasco1992 <hector.carrasco@codeimmersives.com>
2022-02-01 14:46:39 -0500

    adding log in and sign up using firebase

--- src/index.js
+++ src/index.js
@@ -1,13 +1,17 @@
 import Phaser from "phaser";
 import config from "./config/config";
+import { initializeApp } from "firebase/app";
+import Firebase from "firebase/app";
+import firebaseConfig from "./config/fireBaseConfig";
 
 class Game extends Phaser.Game {
   constructor() {
     super(config);
-    this.scene.start("StartScene");
+    this.scene.start("Menu");
   }
 }
 
+const app = initializeApp(firebaseConfig);
 window.onload = function () {
   window.game = new Game();
 };
