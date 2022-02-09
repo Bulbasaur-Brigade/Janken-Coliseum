ec2a60815fa875c75f8b839832575bd6cd9e7d8e
Elstan Lewis <elstanlewis7@gmail.com>
2022-02-09 15:45:23 -0500

    make it better

--- webpack/base.js
+++ webpack/base.js
@@ -1,12 +1,12 @@
-const webpack = require("webpack");
-const path = require("path");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
-const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const webpack = require('webpack');
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 // const nodeExternals = require("webpack-node-externals");
 
 module.exports = {
-  mode: "development",
-  devtool: "eval-source-map",
+  mode: 'production',
+  devtool: 'eval-source-map',
   module: {
     // externals: [nodeExternals()],
     rules: [
@@ -14,29 +14,29 @@ module.exports = {
         test: /\.js$/,
         exclude: /node_modules/,
         use: {
-          loader: "babel-loader",
+          loader: 'babel-loader',
         },
       },
       {
         test: [/\.vert$/, /\.frag$/],
-        use: "raw-loader",
+        use: 'raw-loader',
       },
       {
         test: /\.(gif|png|jpe?g|svg|xml)$/i,
-        use: "file-loader",
+        use: 'file-loader',
       },
     ],
   },
   plugins: [
     new CleanWebpackPlugin({
-      root: path.resolve(__dirname, "../"),
+      root: path.resolve(__dirname, '../'),
     }),
     new webpack.DefinePlugin({
       CANVAS_RENDERER: JSON.stringify(true),
       WEBGL_RENDERER: JSON.stringify(true),
     }),
     new HtmlWebpackPlugin({
-      template: "./index.html",
+      template: './index.html',
     }),
   ],
 };
