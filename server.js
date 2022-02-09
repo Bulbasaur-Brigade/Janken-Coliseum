ad1bbeeed865f30738103e5e675940b030985aa8
Elstan Lewis <elstanlewis7@gmail.com>
2022-02-09 15:44:21 -0500

    make it better

--- server.js
+++ server.js
@@ -1,38 +1,38 @@
-// const server = require("express")();
-// const http = require("http").createServer(server);
-// const cors = require("cors");
+const server = require('express')();
+const http = require('http').createServer(server);
+const cors = require('cors');
 
-// const players = {};
-// const io = require("socket.io")(http, {
-//   cors: {
-//     origin: "https://localhost:8080",
+const players = {};
+const io = require('socket.io')(http, {
+  cors: {
+    origin: 'https://localhost:8080',
 
-//     credentials: true,
-//   },
-// });
+    credentials: true,
+  },
+});
 
-// io.on("connection", (socket) => {
-//   console.log("A user connected: " + socket.id);
+io.on('connection', (socket) => {
+  console.log('A user connected: ' + socket.id);
 
-//   socket.on("disconnect", () => {
-//     console.log("A user disconnected " + socket.id);
-//     delete players[socket.id];
-//     io.emit("disconnect", socket.id);
-//   });
+  socket.on('disconnect', () => {
+    console.log('A user disconnected ' + socket.id);
+    delete players[socket.id];
+    io.emit('disconnect', socket.id);
+  });
 
-//   players[socket.id]({
-//     rotation: 0,
-//     x: 400,
-//     y: 300,
-//     playerId: socket.id,
-//   });
+  players[socket.id]({
+    rotation: 0,
+    x: 400,
+    y: 300,
+    playerId: socket.id,
+  });
 
-//   socket.emit("currentPlayers", players);
+  socket.emit('currentPlayers', players);
 
-//   socket.broadcast.emit("newplayer", players[socket.id]);
-//   console.log(players);
-// });
+  socket.broadcast.emit('newplayer', players[socket.id]);
+  console.log(players);
+});
 
-// http.listen(3000, () => {
-//   console.log("Server started!");
-// });
+http.listen(process.env.Port || 3000, () => {
+  console.log('Server started!');
+});
